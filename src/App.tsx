79013c7580027337aee57ae3d1d257fe4c48d4b2
Madi Samy <samymadi187@gmail.com>
2022-10-16 21:03:27 +0100

    adding material ui and implement the provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ThemeProvider } from '@mui/material';
+
 import './App.css';
+
+
+
+//themes
+import {
+  lightTheme
+} from './style/theme';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+        
+    </ThemeProvider>
   );
 }
 
---
+++ src/style/theme.ts
@@ -0,0 +1,55 @@
+import {
+    createTheme,
+    Theme,
+}   from '@mui/material';
+
+
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    primary: {
+      danger: string;
+    };
+    secondary:{
+
+    }
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    primary?: {
+     
+    };
+    secondary?:{
+
+    }
+  }
+}
+
+const PaletteLight = {
+    primaryMain:'#F93E6C',
+    primaryHover:'#E03861',
+    primaryActive:'#C73256',
+    primaryDisabled:'#FC9FB6',
+
+    secondaryMain:'#2CCFBC',
+    secondaryHover:'#28BAA9',
+    secondaryActive:'#23A696',
+    secondaryDisabled:'#96E7DE'
+}
+
+
+export const lightTheme :Theme =  createTheme({
+    palette:{
+        primary:{
+            main:PaletteLight.primaryMain,
+
+        
+        },
+        secondary:{
+            main:PaletteLight.secondaryMain
+        },
+        
+        
+    },
+    
+})
