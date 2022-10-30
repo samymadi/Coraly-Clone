e2082d055b49d19c5cff71852a9a10b8a3aaed83
Madi Samy <samymadi187@gmail.com>
2022-10-30 12:39:39 +0100

    fixing color input design

--- src/components/Pages/dashboard/process/addNewProcessbodyModal.tsx
+++ src/components/Pages/dashboard/process/addNewProcessbodyModal.tsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react'
 
 
-import { styled } from '@mui/material'
+import { 
+    styled,
+    InputAdornment,
+    useTheme
+ } from '@mui/material'
 
 
 import Input from '../../../shared/input/input'
+
+
+
+//assets
+import {ReactComponent as ColorPalette} from '../../../../assets/icons/color_palette.svg';
 
 
 const PROCESS_NAME='Process name';
 const COLOR='Color';
 
 function AddNewProcessbodyModal() {
+
+
+    const {
+        paletteColor
+    } =  useTheme();
 
 
     const [state1,setState1] = useState('');
@@ -24,12 +38,24 @@ function AddNewProcessbodyModal() {
             sx={{
                 marginBottom:'24px',
             }}/>
-        <Input
-            type='color'
-            label={COLOR}
-            setValue={setState1}
-            />
-    </Container>
+        
+            <ColorInput
+                    type='color'
+                    label={COLOR}
+                    setValue={setState1}
+                    InputProps={{
+                            endAdornment:(<InputAdornment
+                                                position='end'>
+                                                    <ColorPalette
+                                                        style={{
+                                                            position:'absolute',
+                                                            right:'13px'
+                                                        }}
+                                                        fill={paletteColor.neutral['50']}/>
+                                            </InputAdornment>)
+                    }}
+                />
+            </Container>
   )
 }
 
@@ -37,8 +63,32 @@ export default AddNewProcessbodyModal
 
 
 
-const Container = styled('div')(({})=>({
+const Container = styled('div')(()=>({
     display:'flex',
     flexDirection:'column',
     marginBottom:'24px',
+}))
+
+
+const ColorInput = styled(Input)(({theme})=>({
+    
+
+    "& input":{
+        "-webkit-appearance": "none",
+        appearance:'none',
+        padding:"0", 
+        paddingLeft:"10px",
+        width: "27px",
+        height: "30px",  
+         
+        "&::-webkit-color-swatch":{
+            border: "none",
+            borderRadius:'50%',
+           
+            
+        },
+        
+    }
+    
+
 }))
